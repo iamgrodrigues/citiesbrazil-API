577f81ee711f10414a207e5a3219442cd356b15e
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-07 20:44:43 -0300

    first commit

---
+++ src/test/java/com/iamgrodrigues/citiesbrazilapi/CitiesbrazilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamgrodrigues.citiesbrazilapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesbrazilApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
